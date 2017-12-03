28c2b7a78760ac9e65d06d6cf2f8758301db3c5d
Saptarshi Roy Choudhuri <saptarshi.roychoudhuri@gmail.com>
2017-12-03 16:57:40 +0530

    Basic app created. Category fragment and category activity populated

---
+++ app/src/main/java/revels18/in/revels18/application/Revels.java
@@ -0,0 +1,19 @@
+package revels18.in.revels18.application;
+
+import android.app.Application;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class Revels extends Application {
+    public static int searchOpen =0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+
+}
---
+++ app/src/main/java/revels18/in/revels18/fragments/ResultsFragment.java
@@ -0,0 +1,28 @@
+package revels18.in.revels18.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import revels18.in.revels18.R;
+
+public class ResultsFragment extends Fragment {
+    public static ResultsFragment newInstance() {
+        ResultsFragment fragment = new ResultsFragment();
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_results, container, false);
+    }
+}
